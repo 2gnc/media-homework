a6c7b9de15c6246d5af408818470c2d3ffa74822
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-15 22:17:35 +0300

    initial commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+window.addEventListener('DOMContentLoaded', function (e) {
+  var player = document.getElementById('player');
+  var canvas = document.getElementById('canvas');
+
+  var videoOk = function videoOk(stream) {
+    player.srcObject = stream;
+  };
+
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+  }).then(videoOk);
+  canvas.getContext('2d').drawImage(player, 0, 0);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,14 @@
+window.addEventListener( 'DOMContentLoaded', ( e ) => {
 
+  const player = document.getElementById('player');
+  const canvas = document.getElementById('canvas');
 
+  const videoOk = (stream) => {
+    player.srcObject = stream;
+  };
+
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    .then(videoOk);
+
+  canvas.getContext('2d').drawImage(player, 0, 0);
+});
