5bc036e0e0271dbb6fc929582198a7b429113831
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-03-15 11:20:49 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autopref = require( 'gulp-autoprefixer' );
+var babel = require('gulp-babel');
+
+
+gulp.task( 'sass', function() {
+	return gulp.src("src/sass/*.sass")
+		.pipe(sass())
+		.pipe(autopref({
+			browsers: ['last 2 versions'],
+			cascade: false
+			}))
+		.pipe(gulp.dest("public/css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task( 'js', function() {
+	return gulp.src( 'src/js/*.js' )
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(gulp.dest('public/scripts'))
+		.pipe(browserSync.stream());
+});
+
+
+gulp.task( 'browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./public"
+		}
+	});
+
+	gulp.watch("src/sass/*.sass", ['sass']);
+	gulp.watch("src/js/*.js", ['js']);
+	gulp.watch("public/*.html").on('change', browserSync.reload);
+});
