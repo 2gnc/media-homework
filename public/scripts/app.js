4012d6714b68f214d5e1e4712830108519d9cf7c
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-16 00:12:10 +0300

    видео с камеры передается в canvas

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,29 @@
 window.addEventListener('DOMContentLoaded', function (e) {
   var player = document.getElementById('player');
   var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d'); //let vendorURL = window.URL || window.webkitURL;
 
   var videoOk = function videoOk(stream) {
     player.srcObject = stream;
   };
 
+  var videoNO = function videoNO() {
+    var msg = document.createElement('p');
+    msg.innerText = 'Ваш браузер не поддерживает зрение Захватчика, установите Яндекс Браузер';
+    msg.classList.add('msg');
+    document.querySelector('header').appendChild(msg);
+  };
+
+  var draw = function draw(video, context, width, height) {
+    context.drawImage(video, 0, 0, width, height);
+    setTimeout(draw, 10, video, context, width, height);
+  };
+
   navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true
-  }).then(videoOk);
-  canvas.getContext('2d').drawImage(player, 0, 0);
+  }).then(videoOk).catch(videoNO);
+  player.addEventListener('play', function (e) {
+    draw(e.target, context, 300, 200);
+  }, false);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,32 @@ window.addEventListener( 'DOMContentLoaded', ( e ) => {
 
   const player = document.getElementById('player');
   const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  //let vendorURL = window.URL || window.webkitURL;
+
 
   const videoOk = (stream) => {
     player.srcObject = stream;
   };
 
-  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-    .then(videoOk);
+  const videoNO = () => {
+    let msg = document.createElement('p');
+    msg.innerText = 'Ваш браузер не поддерживает зрение Захватчика, установите Яндекс Браузер';
+    msg.classList.add('msg');
+    document.querySelector('header').appendChild(msg);
+  };
 
-  canvas.getContext('2d').drawImage(player, 0, 0);
+  const draw = (video, context, width, height) => {
+    context.drawImage(video, 0, 0, width, height);
+    setTimeout(draw, 10, video, context, width, height);
+  };
+
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    .then(videoOk).catch(videoNO);
+
+  player.addEventListener('play', (e) => {
+    draw(e.target, context, 300, 200);
+  }, false)
+
 });
