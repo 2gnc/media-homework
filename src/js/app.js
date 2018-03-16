945ccf0db1791ac7f30177eeb9a3fd0f4cbc4552
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-16 15:45:01 +0300

    незначистельные изменения

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', (e) => {
+
   const player = document.getElementById('player');
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
