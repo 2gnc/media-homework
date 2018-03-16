c1e2cd9692f33c35853103c7afb1cd24bde83820
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-16 17:30:49 +0300

    вывела изображение с камеры в canvas

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,27 @@ window.addEventListener('DOMContentLoaded', (e) => {
   const context = canvas.getContext('2d');
   const ui = document.querySelector('ui__robo-ui');
 
-  //   const videoOk = (stream) => {
-  //     player.srcObject = stream;
-  //   };
-  //
+  let canvasWidth = canvas.clientWidth;
+  let canvasHeight = canvas.clientHeight;
+
+  let resizeTimeout;
+
+  const getCanvasDimentions = () => {
+    canvasWidth = canvas.clientWidth;
+    canvasHeight = canvas.clientHeight;
+  };
+
+  const resizeThrottler = () => {
+    if ( !resizeTimeout ) {
+      resizeTimeout = setTimeout(function() {
+        resizeTimeout = null;
+        getCanvasDimentions();
+      }, 300);
+    }
+  };
+
+  window.addEventListener( 'resize', resizeThrottler, false );
+
   const displayErrorMgs = (msg) => {
     const message = document.createElement('p');
     message.innerText = msg;
@@ -22,49 +39,45 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
   const displayUI = () => {
     getDevices();
-
   }
 
   const getDevices = () => {
     navigator.mediaDevices.enumerateDevices()
       .then( (devices) => {
-
         let cameras = [];
-
         devices.forEach( (device, i) => {
-
           if (device.kind === 'videoinput') {
             cameras.push( device );
           }
-
         });
-
         if(cameras.length === 0) {
           displayErrorMgs('Ваш захватчик не оснащен камерой');
         } else {
           let camsForUi = document.createElement('div');
           camsForUi.classList.add('ui__devices');
         }
-        console.log( cameras.length );
-
       })
       .catch( () => {
         console.log( 'не удалось получить список устройств' );
     } );
   };
-  //
-  //   const draw = (video, context, width, height) => {
-  //     context.drawImage(video, 0, 0, width, height);
-  //     setTimeout(draw, 10, video, context, width, height);
-  //   };
-  //
-  //   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-  //     .then(videoOk).catch(videoNO);
-  //
-  //   player.addEventListener('play', (e) => {
-  //     draw(e.target, context, 300, 200);
-  //   }, false)
-  //
+
+  const videoOk = (stream) => {
+    player.srcObject = stream;
+  };
+
+
+  const getVideo = (video, cont, width, height) => {
+    cont.drawImage( video, 0, 0, width, height );
+    setTimeout(getVideo, 20, video, cont, width, height)
+  };
+
+  navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true
+    })
+    .then(videoOk)
+    .catch(videoNO);
 
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
   if (!navigator.getUserMedia) {
@@ -72,9 +85,14 @@ window.addEventListener('DOMContentLoaded', (e) => {
   }
 
   if (navigator.mediaDevices || navigator.mediaDevices.enumerateDevices) {
+    player.addEventListener('play', (e) => {
+      getVideo(e.target, context, canvasWidth/3.6, canvasHeight/3.6);
+
+    }, false)
     // запустить видео
     // отрисовать интерфейс
     displayUI();
+
   }
 });
 
