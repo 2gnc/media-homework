3dfe9db051a7d99e34c24bbaf3e8ffd31b43a915
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-16 14:20:54 +0300

    добавила функцию показывающую аудио и видео устроиства для построения ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,62 @@
-window.addEventListener( 'DOMContentLoaded', ( e ) => {
-
+window.addEventListener('DOMContentLoaded', (e) => {
   const player = document.getElementById('player');
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
 
-  //let vendorURL = window.URL || window.webkitURL;
-
-
-  const videoOk = (stream) => {
-    player.srcObject = stream;
-  };
-
+  //   const videoOk = (stream) => {
+  //     player.srcObject = stream;
+  //   };
+  //
   const videoNO = () => {
-    let msg = document.createElement('p');
+    const msg = document.createElement('p');
     msg.innerText = 'Ваш браузер не поддерживает зрение Захватчика, установите Яндекс Браузер';
     msg.classList.add('msg');
     document.querySelector('header').appendChild(msg);
   };
 
-  const draw = (video, context, width, height) => {
-    context.drawImage(video, 0, 0, width, height);
-    setTimeout(draw, 10, video, context, width, height);
+  const getDevices = () => {
+    navigator.mediaDevices.enumerateDevices()
+      .then( (devices) => {
+        let cameras = [];
+        let audio = [];
+        devices.forEach( (device, i) => {
+          if (device.kind === 'videoinput') {
+            cameras.push( device );
+          };
+          if (device.kind === 'audioinput') {
+            audio.push( device );
+          };
+        });
+        console.log( 'video', cameras );
+        console.log( 'audio', audio );
+      })
+      .catch( () => {
+        console.log( 'не удалось получить список устройств' );
+    } );
   };
+  //
+  //   const draw = (video, context, width, height) => {
+  //     context.drawImage(video, 0, 0, width, height);
+  //     setTimeout(draw, 10, video, context, width, height);
+  //   };
+  //
+  //   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+  //     .then(videoOk).catch(videoNO);
+  //
+  //   player.addEventListener('play', (e) => {
+  //     draw(e.target, context, 300, 200);
+  //   }, false)
+  //
 
-  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-    .then(videoOk).catch(videoNO);
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  if (!navigator.getUserMedia) {
+    videoNO();
+  }
 
-  player.addEventListener('play', (e) => {
-    draw(e.target, context, 300, 200);
-  }, false)
-
+  if (navigator.mediaDevices || navigator.mediaDevices.enumerateDevices) {
+    // апустить видео
+    // отрисовать интерфейс
+    getDevices();
+  }
 });
+
