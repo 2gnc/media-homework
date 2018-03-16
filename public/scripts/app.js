b6cbf4858d5195fee80d9b2ffa46d9466f9528a0
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-16 14:21:16 +0300

    оптимизировала стили

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,10 @@
 window.addEventListener('DOMContentLoaded', function (e) {
   var player = document.getElementById('player');
   var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d'); //let vendorURL = window.URL || window.webkitURL;
-
-  var videoOk = function videoOk(stream) {
-    player.srcObject = stream;
-  };
+  var context = canvas.getContext('2d'); //   const videoOk = (stream) => {
+  //     player.srcObject = stream;
+  //   };
+  //
 
   var videoNO = function videoNO() {
     var msg = document.createElement('p');
@@ -16,16 +15,52 @@ window.addEventListener('DOMContentLoaded', function (e) {
     document.querySelector('header').appendChild(msg);
   };
 
-  var draw = function draw(video, context, width, height) {
-    context.drawImage(video, 0, 0, width, height);
-    setTimeout(draw, 10, video, context, width, height);
-  };
+  var getDevices = function getDevices() {
+    navigator.mediaDevices.enumerateDevices().then(function (devices) {
+      var cameras = [];
+      var audio = [];
+      devices.forEach(function (device, i) {
+        if (device.kind === 'videoinput') {
+          cameras.push(device);
+        }
 
-  navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true
-  }).then(videoOk).catch(videoNO);
-  player.addEventListener('play', function (e) {
-    draw(e.target, context, 300, 200);
-  }, false);
+        ;
+
+        if (device.kind === 'audioinput') {
+          audio.push(device);
+        }
+
+        ;
+      });
+      console.log('video', cameras);
+      console.log('audio', audio);
+    }).catch(function () {
+      console.log('не удалось получить список устройств');
+    });
+  }; //
+  //   const draw = (video, context, width, height) => {
+  //     context.drawImage(video, 0, 0, width, height);
+  //     setTimeout(draw, 10, video, context, width, height);
+  //   };
+  //
+  //   navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+  //     .then(videoOk).catch(videoNO);
+  //
+  //   player.addEventListener('play', (e) => {
+  //     draw(e.target, context, 300, 200);
+  //   }, false)
+  //
+
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  if (!navigator.getUserMedia) {
+    videoNO();
+  }
+
+  if (navigator.mediaDevices || navigator.mediaDevices.enumerateDevices) {
+    // апустить видео
+    // отрисовать интерфейс
+    getDevices();
+  }
 });
